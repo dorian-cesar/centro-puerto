6156c1c85fec18c20bde4b705540b597d016dd4c
alexisTobar <tobaralexis.89@gmail.com>
2024-05-14 11:18:07 -0400

    modificacion de toggle

--- js/index.js
+++ js/index.js
@@ -1,11 +1,14 @@
+
+
 const sidebarToggle = document.getElementById('menu-toggle');
 const sidebar = document.getElementById('sidebar');
 
-sidebarToggle.addEventListener('click', () => {
+sidebarToggle.addEventListener('click', toggleSidebar);
+
+function toggleSidebar() {
     sidebar.classList.toggle('active');
     sidebarToggle.classList.toggle('active');
-});
-
+}
 $(document).ready(function () {
 
     /* $("#reservas").load("reservas.html"); */
@@ -13,37 +16,31 @@ $(document).ready(function () {
     $("#about").load("about.html");
     $("#faq").load("faq.html");
 
-    $("#mostrarFaq").click(function () {
-        /* $("#reservas").show(); */
-        $("#faq").show();
-        $("#rutas").hide();
-        $("#about").hide();
+});
 
-        $('#sidebar').toggleClass('active');
-        $(this).toggleClass('active');
+$("#mostrarFaq").click(function () {
+    $("#faq").show();
+    $("#rutas").hide();
+    $("#about").hide();
+    if (sidebar.classList.contains('active')) {
+        toggleSidebar();
+    }
+});
 
-    });
+$("#mostrarRutas").click(function () {
+    $("#faq").hide();
+    $("#rutas").show();
+    $("#about").hide();
+    if (sidebar.classList.contains('active')) {
+        toggleSidebar();
+    }
+});
 
-    $("#mostrarRutas").click(function () {
-        /* $("#reservas").hide(); */
-        $("#faq").hide();
-        $("#rutas").show();
-        $("#about").hide();
-
-        $('#sidebar').toggleClass('active');
-        $(this).toggleClass('active');
-
-    });
-
-    $("#mostrarAbout").click(function () {
-        /* $("#reservas").hide(); */
-        $("#faq").hide();
-        $("#rutas").hide();
-        $("#about").show();
-
-        $('#sidebar').toggleClass('active');
-        $(this).toggleClass('active');
-
-    });
-
+$("#mostrarAbout").click(function () {
+    $("#faq").hide();
+    $("#rutas").hide();
+    $("#about").show();
+    if (sidebar.classList.contains('active')) {
+        toggleSidebar();
+    }
 });
